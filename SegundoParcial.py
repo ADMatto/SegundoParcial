469ed9e256559ccaf44e48fd3e5d4d3bd3aec2c7
ADMatto <adrian.matto@gmail.com>
2020-11-02 18:55:30 -0300

    Archivos del parcial

---
+++ SegundoParcial.py
@@ -0,0 +1,9 @@
+def menu():
+    print('Opciones: \n 1) Cargar Datos\n2)\n3)')
+
+def cargar_Datos():
+    try:
+        pass
+    except IOError:
+        print('Hubo un error al abrir el archivo!')
+
